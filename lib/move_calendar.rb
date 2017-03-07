64e2f4f6ddd62f699596a87c521e48d413c4e22c
racheltstevens <racheltstevens@gmail.com>
2017-03-07 09:20:58 -0500

    Moves calendar a certain number of weeks.
    Still does not account for snow days/holidays.

--- lib/move_calendar.rb
+++ lib/move_calendar.rb
@@ -3,7 +3,7 @@ require_relative "./ics_calendar.rb"
 def move_calendar(input_file, output_file, interval_days)
   calendar = IcsCalendar.new(input_file)
   puts calendar.ics_events.first.start_date
-  calendar.move(2)
+  calendar.move(interval_days)
   puts calendar.ics_events.first.start_date
   calendar.write_calendar(output_file)
 end
